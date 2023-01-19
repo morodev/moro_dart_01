dd54ea0780748ea27e8b902a0ccb684fa9e55850
Luca Moro <luca.moro@spindox.it>
2023-01-19 21:35:01 +0100

    first commit

---
+++ bin/moro_dart_01.dart
@@ -0,0 +1,5 @@
+import 'package:moro_dart_01/moro_dart_01.dart' as moro_dart_01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${moro_dart_01.calculate()}!');
+}
---
+++ test/moro_dart_01_test.dart
@@ -0,0 +1,8 @@
+import 'package:moro_dart_01/moro_dart_01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
